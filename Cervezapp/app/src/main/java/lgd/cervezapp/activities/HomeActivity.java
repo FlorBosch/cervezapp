505181c83b96d2f5446af18650e68f315fc2ef88
Pablo Musumeci <pablo.musumeci@mulesoft.com>
2016-01-23 17:00:20 -0300

    New folder structure

--- Cervezapp/app/src/main/java/lgd/cervezapp/activities/HomeActivity.java
+++ Cervezapp/app/src/main/java/lgd/cervezapp/activities/HomeActivity.java
@@ -1,9 +1,11 @@
-package lgd.cervezapp;
+package lgd.cervezapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import lgd.cervezapp.R;
 
 public class HomeActivity extends AppCompatActivity {
 
